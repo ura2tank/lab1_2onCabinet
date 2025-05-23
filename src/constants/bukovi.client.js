cf6187696d41cc4d8eb821ca2935316873dd2ae6
ura2tank <ura2tank@mail.ru>
2025-05-23 23:40:44 +0700

    изменены data.json и bukovi

--- src/constants/bukovi.client.js
+++ src/constants/bukovi.client.js
@@ -2,35 +2,22 @@ console.log("a");
 
 async function updateAlphabetInHtml() {
     try {
-        console.log("Начинаем загрузку данных...");
+        // console.log("загрузкf");
         const response = await fetch('/constants/data.json');
-        console.log("Получен ответ:", response.status);
+        // console.log(response.status);
         
         const data = await response.json();
-        console.log("Данные загружены:", data);
+        // console.log(data);
 
-        document.title = data.title;
-
-        const nameAlfabet = document.getElementById('name_alfabet');
-        const lowercase = document.getElementById('lowercase');
-        const uppercase = document.getElementById('uppercase');
-
-
-        if (nameAlfabet) {
-            nameAlfabet.innerHTML = `${data.name_alfabet}`;
-        }
-        
-        if (lowercase) {
-            lowercase.innerHTML = `${data.lowercase}`;
-        }
-        
-        if (uppercase) {
-            uppercase.innerHTML = `${data.uppercase}`;
-        }
+        Object.entries(data.elements).forEach(([id, value]) => {
+            const element = document.getElementById(id);
+            if (element) {
+                element.innerHTML = value;
+            }
+        });
     } catch (error) {
         console.error('Ошибка при обновлении HTML:', error);
     }
 }
-
 
 document.addEventListener('DOMContentLoaded', updateAlphabetInHtml); 
